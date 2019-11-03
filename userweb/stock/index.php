aa6c882067a1bb9e19f9cf244823bd24339d373e
kiichigonokami <info@kiichigo.work>
2019-11-03 23:32:26 +0900

    fix

---
+++ userweb/stock/index.php
@@ -0,0 +1,6 @@
+<?php
+$stock_id = 'RELAXES';
+//$Path = 'https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910TOKU/';
+$Path = './img/';
+include('../loop.php');
+?>
---
+++ userweb/stock/pc_loop.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php
+$site_url = 'https://yoihibi.jp/';
+$timestamp = date('YmdHis');
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta name="format-detection" content="telephone=no" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Cache-Control" content="no-cache" />
+<title><?php echo $site_title ?> | よい日々ショップ</title>
+<link href="<?php echo $site_url ?>/client_info/FLORAHOUSE01/view/userweb/css/style.css?timestamp=<?php echo $timestamp ?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo $site_url ?>/client_info/FLORAHOUSE01/view/userweb/favicon.ico?timestamp=<?php echo $timestamp ?>" rel="shortcut icon" />
+
+<script src="<?php echo $site_url ?>/client_info/FLORAHOUSE01/view/userweb/js/ebisu_lib.js?timestamp=<?php echo $timestamp ?>" type="text/javascript" charset="utf-8"></script>
+<script src="<?php echo $site_url ?>/client_info/FLORAHOUSE01/view/userweb/js/ebisu.js?timestamp=<?php echo $timestamp ?>" type="text/javascript" charset="utf-8"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous" />
+<meta name="keywords" content="【読みもの】<?php echo $site_title ?>" /><meta property="og:description" content="" /><meta property="og:title" content="【読みもの】<?php echo $site_title ?>" /><meta property="og:image" content="https://dbcn1bdvswqbx.cloudfront.net/client_info/FLORAHOUSE01/itemimage/1812TOKU/other01.png" /><meta property="og:type" content="product" /><meta property="og:url" content="https://yoihibi.jp/item/1812TOKU.html" /><meta property="og:site_name" content="よい日々ショップ" /><link rel="canonical" href="https://yoihibi.jp/item/1812TOKU.html" /></head>
+<body>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery.matchHeight/0.7.2/jquery.matchHeight-min.js"></script>
+<script src="<?php echo $site_url ?>/client_info/FLORAHOUSE01/view/userweb/js/common.js?timestamp=<?php echo $timestamp ?>" type="text/javascript" charset="utf-8"></script>
+
+<!-- header -->
+<header class="global-header global-header-home" id="js-global-header">
+  <div class="global-header-wrap">
+	<div class="global-header-inner">
+		<h1>わたしのけんこうをデザインする</h1>
+        <div class="h_logo"><a href="https://yoihibi.jp/"><img src="//dbcn1bdvswqbx.cloudfront.net/client_info/FLORAHOUSE01/view/userweb/images/common/header/h_logo.png?timestamp=<?php echo $timestamp ?>" /></a></div>
+    <div class="h_logo_s"><a href="https://yoihibi.jp/"><img src="//dbcn1bdvswqbx.cloudfront.net/client_info/FLORAHOUSE01/view/userweb/images/common/header/h_logo_s.png?timestamp=<?php echo $timestamp ?>" /></a></div>
+
+        <div class="global-header-infoarea">
+        	<dl class="header-tel">
+              <dt>お電話でのご注文・お問い合わせはこちら</dt>
+              <dd class="free_phone">0120-775-669</dd>
+              <dd>受付時間 9:30 ～ 18:00（土日祝を除く）</dd>
+            </dl>
+
+            <div class="header-cart">
+                <a href="https://shop-florahouse.jp/"><img src="//dbcn1bdvswqbx.cloudfront.net/client_info/FLORAHOUSE01/view/userweb/images/common/parts/icon_cart_w.png?timestamp=<?php echo $timestamp ?>" class="" /><p>カートを見る</p></a>
+        </div>
+
+            <ul class="header-guide">
+              <li><a href="https://yoihibi.jp/ext/guide.html" class="h_navi_guide">ご利用ガイド</a></li>
+              <li><a href="https://yoihibi.jp/ext/guide.html?id=faq" target="_blank" class="h_navi_faq">よくあるご質問</a></li>
+            </ul>
+        </div>
+	</div>
+  </div>
+    <nav class="global-laptop-nav">
+      <div class="l_inner">
+        <ul id="EBISU_MENU_LIST">
+          <li class="navi_okaimono"><a href="https://yoihibi.jp/" class="top" menuName="top" selectedClass="top_select">お買いもの</a></li>
+          <li class="navi_yomimono"><a href="https://yoihibi.jp/category/Y_ALL/" class="y" menuName="y" selectedClass="y_select">読みもの</a></li>
+          <li class="navi_mypage"><a href="https://shop-florahouse.jp/account/my_page_login" class="mypage" menuName="mypage" selectedClass="mypage_select">マイページ</a></li>
+        </ul>
+
+        <div id="search">
+            <form action="https://yoihibi.jp/item_list.html" method="get" name="keyword_search_form"><input name="siborikomi_clear" value="1" type="hidden" />
+                <input name="keyword" id="form" value="" placeholder="キーワード検索" class="form" type="text" />
+                <input type="submit" class="button positive small" value="" />
+            </form>
+        </div>
+      </div>
+    </nav>
+</header>
+<!-- / header -->
+
+
+<script src="<?php echo $site_url ?>/client_info/FLORAHOUSE01/view/userweb/js/replace_menu_class.js?timestamp=<?php echo $timestamp ?>" type="text/javascript"></script>
+
+<script src="<?php echo $site_url ?>/client_info/FLORAHOUSE01/view/userweb/js/jquery.leanModal.min.js?timestamp=<?php echo $timestamp ?>"></script>
+<script src="<?php echo $site_url ?>/client_info/FLORAHOUSE01/view/userweb/js/itemdetail.js?timestamp=<?php echo $timestamp ?>"></script>
+
+	<!-- 商品か記事かの判断用出力 -->
+    <div class="pageLayout_judge_area">
+    	<span class="pageLayout_judge">js_flag_ArticleDetail</span>
+    </div>
+
+    <!-- 読みもの用 詳細 -->
+    <div class="itemDetail__article js_flag_ArticleDetail">
+        <!-- container -->
+        <div id="container">
+
+
+        <!-- =============================================================================== -->
+        <!-- ==================メインコンテンツ ================================================== -->
+        <!-- =============================================================================== -->
+            <div id="main" class="no-sidebar">
+
+                <!-- breadcrumbs  -->
+                <div id="breadcrumbs">
+                    <a href="https://yoihibi.jp/">トップ</a>
+
+                     ＞ 【読みもの】<?php echo $site_title ?>
+                </div>
+                <!-- / breadcrumbs -->
+
+
+                <!-- itemDetail -->
+                <div id="itemDetail">
+
+                  <div class="article_detail l_columns_w__wide">
+                    <div class="article_detail__inner">
+                      <?php if (isset($cam)) : ?>
+                      <div class="article_detail_title">
+          							<p><img src="//d2mxjn1ymx4y84.cloudfront.net/client_info/FLORAHOUSE01/view/userweb/images/yomimono_title.png?timestamp=1561532368000" alt=""></p>
+          						</div>
+                      <?php endif; ?>
+
+                        <div class="article_detail_header">
+                            <p class="article_detail_day">
+                                2018年12月1日
+                            </p>
+                            <h2 class="item_detail_ttl_name">【読みもの】<?php echo $site_title ?></h2>
+                            <ul class="hash_label">
+                              <li>
+                                  #血管年齢
+                              </li>
+
+
+                            </ul>
+                        </div>
+
+                        <div class="article_detail_info">
+                          <?php include('' . $stock_id . '/parts/2.php') ?>
+                        </div>
+
+                        <div class="article_detail_contents">
+                          <?php include('' . $stock_id . '/parts/3.php') ?>
+                        </div>
+
+                    </div>
+                  </div><!--//item_detail_main l_columns_w-->
+
+                  <div class="article_detail_person">
+
+                        <!-- デザインパターン出し分け -->
+                        <div class="pageLayout_judge_area">
+                            <span class="pageLayout_judge">pattern_a</span>
+                        </div>
+
+                    <div class="article_detail_person__coverwrap">
+                        <?php include('' . $stock_id . '/parts/1.php') ?>
+                    </div>
+
+                  </div>
+
+                  <div class="link_area l_flex mb40">
+                        <a href="https://yoihibi.jp/category/Y_ALL/" class="button button__brown center_block arrow arrow_right__w">読みものトップに戻る</a>
+                        <a href="https://yoihibi.jp/category/Y/" class="button button__brown center_block arrow arrow_right__w">他の読みものを検索する</a>
+                  </div>
+
+                </div><!-- / itemDetail -->
+            </div><!-- /main -->
+        </div><!-- / container -->
+    </div><!--//itemDetail__article-->
+
+
+
+
+<!-- ==================フッター ================================================== -->
+<script src="<?php echo $site_url ?>/client_info/FLORAHOUSE01/view/userweb/js/footerFixed.js?timestamp=<?php echo $timestamp ?>" type="text/javascript"></script>
+<script type="text/javascript">
+//<![CDATA[
+$(function(){
+	$(".topBtn").hover(function () {
+		$(".topBtn").addClass("page_top_over");
+	},
+	function () {
+		$(".topBtn").removeClass("page_top_over");
+	}
+	);
+});
+//]]>
+</script>
+
+<div id="footer">
+    <p class="topBtn" id="topBtn">
+        <a href="#"><img src="//dbcn1bdvswqbx.cloudfront.net/client_info/FLORAHOUSE01/view/userweb/images/common/footer/page_top.png?timestamp=<?php echo $timestamp ?>" alt="ページトップへ戻る" /></a>
+    </p>
+
+	<div class="footer_inqarea">
+    	<div class="footer_inq__order">
+        	<dl>
+              <dt>ご注文・お支払いについて</dt>
+              <dd class="free_phone">0120-775-669</dd>
+              <dd class="time"><span>受付時間</span>9:30-18:00(土日祝を除く)</dd>
+            </dl>
+        </div>
+
+        <div class="footer_inq__consultation">
+        	<dl>
+              <dt>商品のご相談について</dt>
+              <dd class="free_phone">0120-167-595</dd>
+              <dd class="time"><span>受付時間</span>9:30-18:00(土日祝を除く)</dd>
+            </dl>
+        </div>
+    </div>
+
+    <div class="fooger_nav">
+    	<ul>
+          <li><a href="https://yoihibi.jp/ext/tokushou.html">特定商取引法に基づく表記</a></li>
+          <li><a href="https://yoihibi.jp/ext/privacy.html">個人情報保護方針</a></li>
+          <li><a href="https://yoihibi.jp/ext/privacy.html?id=agreement">利用規約</a></li>
+          <li><a href="https://yoihibi.jp/ext/about.html">よい日々ショップとは</a></li>
+          <li><a href="https://shop-florahouse.jp/contact/index?_ga=2.120342600.1146823260.1520210302-670963944.1519865099" target="_blank">お問い合わせ</a></li>
+          <!-- <li><a href="ext/guide.html">メルマガ登録</a></li> -->
+        </ul>
+    </div>
+
+	<p class="copyright">Copyright&copy;yoihibi All Rights Reserved.</p>
+</div>
+
+
+
+
+
+<script type="text/javascript">
+//<![CDATA[
+eb$(document).ready(function(){
+    eb$("#topBtn").hide();
+    eb$(window).on("scroll", function() {
+        if (eb$(this).scrollTop() > 100) {
+            eb$("#topBtn").fadeIn("fast");
+        } else {
+            eb$("#topBtn").fadeOut("fast");
+        }
+        scrollHeight = eb$(document).height(); //ドキュメントの高さ
+        scrollPosition = eb$(window).height() + eb$(window).scrollTop(); //現在地
+        footHeight = eb$("#footer").innerHeight(); //footerの高さ（＝止めたい位置）
+        if ( scrollHeight - scrollPosition  <= footHeight ) { //ドキュメントの高さと現在地の差がfooterの高さ以下になったら
+            eb$("#topBtn").css({
+                "position":"absolute", //pisitionをabsolute（親：wrapperからの絶対値）に変更
+                "bottom": footHeight - 7 //下からfooterの高さ + 20px上げた位置に配置
+            });
+        } else { //それ以外の場合は
+            eb$("#topBtn").css({
+                "position":"fixed", //固定表示
+                "bottom": "-7px" //下から20px上げた位置に
+            });
+        }
+    });
+    eb$('#topBtn').click(function () {
+        eb$('body,html').animate({
+        scrollTop: 0
+        }, 400);
+        return false;
+    });
+});
+//]]>
+</script>
+
+<!-- ==================フッター END================================================== -->
+
+
+</body>
+</html>
