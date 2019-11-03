0ee4d5d41f5e4f5202e9a8f999ce693363b87be2
kiichigonokami <info@kiichigo.work>
2019-11-04 00:53:31 +0900

    fix

--- userweb/stock/RELAXES/2.php
+++ userweb/stock/RELAXES/2.php
@@ -3,19 +3,41 @@
   .spnone {display:block!important;}
   .pcnone {display:none!important;}
   .marker {
-    background: -webkit-linear-gradient(transparent 90%, rgba(248, 184, 86, 0.9) 10%);
-    background: -o-linear-gradient(transparent 90%, rgba(248, 184, 86, 0.9) 10%);
-    background: linear-gradient(rgba(0, 0, 0, 0) 90%, rgba(248, 184, 86, 0.9) 10%);
+    background: -webkit-linear-gradient(transparent 30%, rgba(255, 250, 180, 0.9) 60%);
+    background: -o-linear-gradient(transparent 30%, rgba(255, 250, 180, 0.9) 60%);
+    background: linear-gradient(rgba(0, 0, 0, 0) 30%, rgba(255, 250, 180, 0.9) 60%);
   }
+  .explanation {display: block;}
+  .explanation-bigtitle {display: block; margin:0 0 24px 0; font-size: 24px;}
+  .explanation-bigtitle small { display: block; font-size:80%;}
+  .explanation-bigtitle b { font-size:120%; line-height:1.0;}
+  .explanation-bigtitle i { font-style: normal; font-size:120%; color:#E2EDBA;}
+  .explanation-box img { width:100%; display: block; text-align:center;}
+  .explanation-minititle {color:#3D468E; font-weight:bold; font-size:20px; margin:0 0 8px 0;}
+  .explanation-text { font-size:16px; margin:0 0 40px 0; text-align:justify;}
+  .explanation-human {}
+  .explanation-human b {}
+  .explanation-human small {}
   .fl-box {display: flex; flex-wrap: wrap; justify-content: space-around; box-sizing: border-box;}
+  .fl-l {display: block; width:50%; box-sizing: border-box; padding:0 0 0 0;}
+  .fl-r {display: block; width:50%; box-sizing: border-box; padding:0 0 0 16px;}
+  .fl-l2 {display: block; width:75%; box-sizing: border-box; padding:0 0 0 0;}
+  .fl-r2 {display: block; width:25%; box-sizing: border-box; padding:0 0 0 16px;}
   @media screen and (max-width: 767px) {
     .spnone {display:none!important;}
     .pcnone {display:block!important;}
+    .fl-l,
+    .fl-l2,
+    .fl-r2 {display: block; width:100%; box-sizing: border-box; padding:0 16px 0;}
+    .fl-r {display: block; width:100%; box-sizing: border-box; padding:0 0 40px 0;}
+    .explanation-bigtitle {padding:16px 16px 0;}
+    .explanation-minititle {padding:0 16px 0;}
+    .explanation-human {text-align:center; margin:0 0 40px 0;}
+    .explanation-human img { width:140px; margin:0 auto;}
   }
 </style>
 
 <div class="item_detail_section_wrap2">
-  <h2 class="ttl_flower"></h2>
   <div class="l_columns_w item_detail_sub_box">
     <div class="item_detail_section">
 
@@ -23,30 +45,39 @@
 
         <div class="explanation-bigtitle">
           <small>りんごのほのかな甘さと、カモミールなど伝統的な6種のハーブでリラックス</small>
-          <b>明日</b>の<b>元気</b>を育む、<b><i>"</i>質のよいおやすみ<i>"</i></b>を。
+          <b>明日</b>の<b>元気</b>を育む、<b><i>“</i>質のよいおやすみ<i>”</i></b>を。
         </div>
 
         <div class="explanation-box">
+          
           <div class="explanation-minititle cap1">
             いつのまにかたまるストレスをリセットしたい！<br>
             癒しの時間に誘うハーブのカ
           </div>
-          <div class="explanation-text">
-            たくさんのストレスを抱えてしまう、私たちの毎日。なぜかイライラや不安が募り、朝からどんより、気持ちをコントロールできない。そんな時は、心もからだもオーバーワーク気味かもしれません。バランスを取り戻すためには、心地よくリラックスした休息が必要です。
-            <span class="marker">　『リラックス・エッセンス』は、ほのかな甘さのオーガニックアップルジュースに伝統的な6種のやすらぎハーブ、ハイビスカス、パッションフルーツがブレンドされだ"休息サポートドリング"。</span>おやすみ前の習慣にとりいれると、穏やかなリラックスタイムに誘います。
+
+          <div class="fl-box">
+            <div class="explanation-text fl-l">
+              たくさんのストレスを抱えてしまう、私たちの毎日。なぜかイライラや不安が募り、朝からどんより、気持ちをコントロールできない。そんな時は、心もからだもオーバーワーク気味かもしれません。バランスを取り戻すためには、心地よくリラックスした休息が必要です。
+              <span class="marker">　『リラックス・エッセンス』は、ほのかな甘さのオーガニックアップルジュースに伝統的な6種のやすらぎハーブ、ハイビスカス、パッションフルーツがブレンドされだ"休息サポートドリング"。</span>おやすみ前の習慣にとりいれると、穏やかなリラックスタイムに誘います。
+            </div>
+
+            <div class="fl-r">
+              <img src="<?php echo $Path ?>/1.png">
+            </div>
           </div>
 
           <div class="explanation-minititle cap2">
             国際品質保証機関<small>(QAI)</small>認定！オーガニックの品質にこだわるフローラ製
           </div>
           <div class="fl-box">
-            <div class="explanation-text">
+            <div class="explanation-text fl-l2">
               <span class="marker">　ハーブは複数を適切にブレンドすることで、単体では得られない力を発揮</span>して心身に働きかけます。また、<span class="marker">それぞれのハーブの摂取鍼を減らせるので、ネガティブな作用を抑えることができるのもブレンドのメリット</span>です。『リラックス・エッセンス』は、オーバーワークを癒やし、心身のバランスを穏やかに整えてくれるでしょう。
               「心身の浄化」を掲げるフローラの製品はすべて、米国食品医薬局が定めたGMP（医薬品品質管理規則）やカナダ保健省の監督基準に従い、<span class="marker">医薬品と同レベルの厳しさで製造・管理</span>されています。国際品質保証機関<small>(QAI)</small>の認証を受けており、<span class="marker">一切の化学薬品が加わっていないことが証明</span>されています。
             </div>
-            <div class="explanation-human">
+            <div class="explanation-human fl-r2">
+              <img src="<?php echo $Path ?>/2.png">
               <small>フローラナチュラルヘルスカウンセラー<br>
-              ホメオバシ療養士(DHM)<br><small>
+              ホメオバシ療養士(DHM)<br></small>
               ピーター・V・クエンター<small>さん</small>
             </div>
           </div>
