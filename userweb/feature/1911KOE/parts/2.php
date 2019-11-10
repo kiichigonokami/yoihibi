b40f94c8aa4eeab390a75b04ccbfb790e306192e
kiichigonokami <info@kiichigo.work>
2019-11-10 10:30:59 +0900

    fix

---
+++ userweb/feature/1911KOE/parts/2.php
@@ -0,0 +1,152 @@
+<!--★削除禁止★ -->
+<style>
+.spnone {display:block!important;}
+.pcnone {display:none!important;}
+
+/*上書き*/
+.item_detail_ttl_name { border-color: #ef7d68!important;}
+.article_detail_info {margin: 0; padding: 0; width: 100%;}
+
+.marker {
+  background: -webkit-linear-gradient(transparent 90%, rgba(62, 177, 52, 0.9) 10%);
+  background: -o-linear-gradient(transparent 90%, rgba(62, 177, 52, 0.9) 10%);
+  background: linear-gradient(rgba(0, 0, 0, 0) 90%, rgba(62, 177, 52, 0.9) 10%);
+}
+
+/*オリジナル*/
+.column_head { position: relative;}
+.intoroduction { display: block; width: 100%; line-height: 1.6; font-size: 18px; margin: 24px 0 0 0;}
+
+.column_100img {display: block; overflow: hidden; width: 100%; margin-bottom: 20px;}
+.column_100img img { width: 100%;}
+
+.fl-box {display: flex; flex-wrap: wrap; justify-content: space-around;}
+.fl-l {display: block;}
+.fl-r {display: block;}
+.l-1 {width:70%; margin-right:2%;}
+.l-1-l {width:36%; margin-right:4%;}
+.l-1-r {width:60%;}
+.r-1 {width:28%;}
+.l-2 {width:74%; margin-right:2%;}
+.l-2-3 { width: 32%;}
+.r-2 {width:24%; padding:48px 0 0 0;}
+
+.sec-free { display: block; margin: 0 0 0 0; padding: 16px 0;}
+.sec-free-ttl { border-top: 1px solid #ccc; border-bottom: 1px solid #ccc; padding: 14px 0 8px; font-weight: bold; font-size: 24px; color:#e62e8b; margin: 16px 0 32px 0;}
+
+/*おすすめ商品*/
+.camp_stock_area {box-sizing: border-box; width: 100%; clear: both; display: block; margin: 20px 0 0 0; padding: 10px; background: #E9EDDA; border: 1px solid #ddd; overflow: hidden; border-radius: 10px;}
+.camp_stock { width: 25%; float: left;}
+.camp_stock_img { border: 1px solid #ddd; width: 90%; margin: 10px auto 0; height: 180px; display: block; overflow: hidden; text-align: center; background: #fff;}
+.camp_stock_img img { width: auto; height: 100%; text-align: center;}
+.camp_stock_txt { display: block; overflow: hidden; height: 4.5em; margin: 0 auto 0; padding: 10px 0; width: 85%; font-size: 14px; color: #ef7d68; font-weight: bold; text-align: justify;}
+.camp_stock a:hover { opacity: 0.6;}
+
+@media screen and (max-width: 767px) {
+.spnone {display:none!important;}
+.pcnone {display:block!important;}
+
+.intoroduction {font-size: 16px; margin: 16px 0 0 0;}
+
+.sec-free { display: block; margin: 0 0 0 0; padding: 16px 0;}
+.sec-free-ttl { border-top: 1px solid #ccc; border-bottom: 1px solid #ccc; padding: 14px 0 8px; font-weight: bold; font-size: 18px; color:#e62e8b; margin: 16px 0 32px 0;}
+
+.fl-box {display: flex; flex-wrap: wrap; justify-content: space-around;}
+.fl-l {display: block; width: 100%; margin: 0;}
+.fl-r {display: block; width: 100%; margin: 0; padding: 0;}
+.l-2-3 { width: 100%; margin-bottom: 40px;}
+
+#toku-ttl2 { background: url('https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/sp12.jpg') center top 16% no-repeat; background-size: 100%;}
+
+/*おすすめ商品*/
+.camp_stock { width: 50%!important;}
+.camp_stock_img {height: 100px;}
+.camp_stock_txt {font-size: 12px; padding: 0 0 10px 0; height: 2.5em;}
+
+/*リスト*/
+.article_detail_text {display: block; margin: 16px 0 0 0;}
+.article_detail_mokuji{margin: 0 0 100px;}
+.article_detail_mokuji p img{width: 100%; margin: 40px 0;}
+.article_detail_mokuji ul{margin: 0 0 0 30px; display: block;}
+.article_detail_mokuji ul li{color: #348669; text-decoration: underline; font-weight: bold; margin: 0 0 20px;}
+.article_detail_mokuji ul li {list-style-type: none;}
+.article_detail_mokuji ul li::before {position: relative; top: -2px; left: -18px; display: inline-block; width: 6px; height: 6px; content: ''; border-radius: 100%; background: #348669;}
+}
+</style>
+<!--★削除禁止★ -->
+
+<div class="column_head">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/title.png" class="spnone">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/title_s.png" class="pcnone">
+  <div class="intoroduction">
+寒さ対策は万全ですか？ 声結びでは、からだが芯から温まる遠赤外線商品について３号連続で特集します。今月は、電気を使わない「アルファウェーブ」。アルファウェーブは、「使い始めてライフスタイルが変わった」というお声をいただく人気シリーズです。他の皆さんの使い方を参考に、今年の冬は冷え知らずで過ごしませんか？
+  </div>
+</div>
+
+<div class="article_detail_mokuji">
+  <p><img src="//d2mxjn1ymx4y84.cloudfront.net/client_info/FLORAHOUSE01/itemimage/1811OGAT/yomimono_mokuji.png" alt=""></p>
+  <ul>
+    <a href="#toku-ttl1"><li>スクエアパッド｜5層キルティング構造で温かさが持続</li></a>
+    <a href="#toku-ttl2"><li>パイルケット｜夏はタオルケット、冬は毛布感覚で使える</li></a>
+    <a href="#toku-ttl4"><li>肩パッド・ロング｜肩と背中にしっかりフィット</li></a>
+    <a href="#toku-ttl5"><li>サポート腹巻｜薄手でよく伸び、お腹を包み込む</li></a>
+    <a href="#toku-ttl5"><li>スパッツ｜程よいフィット感で下半身から温める</li></a>
+    <a href="#toku-ttl3"><li>「アルファウェーブ」温かさのメカニズム</li></a>
+  </ul>
+</div>
+
+<div class="column_100img" id="toku-ttl1">
+	<img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/pc1.png" class="spnone" alt="スクエアパッド｜5層キルティング構造で温かさが持続｜アルファウエーブ">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/sp1.png" class="pcnone" alt="スクエアパッド｜5層キルティング構造で温かさが持続｜アルファウエーブ">
+</div>
+
+<div class="column_100img" id="toku-ttl2">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/pc2.png" class="spnone" alt="パイルケット｜夏はタオルケット、冬は毛布感覚で使える｜アルファウエーブ">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/sp2.png" class="pcnone" alt="パイルケット｜夏はタオルケット、冬は毛布感覚で使える｜アルファウエーブ">
+</div>
+
+<div class="column_100img" id="toku-ttl4">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/pc4.png" class="spnone" alt="肩パッド・ロング｜肩と背中にしっかりフィット｜アルファウエーブ">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/sp4.png" class="pcnone" alt="肩パッド・ロング｜肩と背中にしっかりフィット｜アルファウエーブ">
+</div>
+
+<div class="column_100img" id="toku-ttl5">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/pc6.png" class="spnone" alt="サポート腹巻｜5層キルティング構造で温かさが持続｜スパッツ｜程よいフィット感で下半身から温める｜アルファウエーブ">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/sp5.png" class="pcnone" alt="サポート腹巻｜5層キルティング構造で温かさが持続｜アルファウエーブ">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/sp6.png" class="pcnone" alt="スパッツ｜程よいフィット感で下半身から温める｜アルファウエーブ">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/sp7.png" class="pcnone" alt="">
+</div>
+
+<div class="column_100img" id="toku-ttl3">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/pc3.png" class="spnone" alt="アルファウェーブ」温かさのメカニズム">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/sp3.png" class="pcnone" alt="アルファウェーブ」温かさのメカニズム">
+</div>
+
+<div class="camp_stock_area">
+
+  <div class="camp_stock"><a href="https://yoihibi.jp/item/ARUSQP.html" target="_blank">
+  <p class="camp_stock_img"><img src="https://dbcn1bdvswqbx.cloudfront.net/client_info/FLORAHOUSE01/itemimage/syouhin/arusqp.jpg"></p>
+  <p class="camp_stock_txt">アルファウェーブ スクエアパッド</p>
+  </a></div>
+
+  <div class="camp_stock"><a href="https://yoihibi.jp/item/ARUPAI.html" target="_blank">
+  <p class="camp_stock_img"><img src="https://dbcn1bdvswqbx.cloudfront.net/client_info/FLORAHOUSE01/itemimage/syouhin/arupai.jpg"></p>
+  <p class="camp_stock_txt">アルファウェーブ パイルケット</p>
+  </a></div>
+
+  <div class="camp_stock"><a href="https://yoihibi.jp/item/ARUKAT.html" target="_blank">
+  <p class="camp_stock_img"><img src="https://dbcn1bdvswqbx.cloudfront.net/client_info/FLORAHOUSE01/itemimage/syouhin/arukat.jpg"></p>
+  <p class="camp_stock_txt">アルファウェーブ 肩パッド・ロング</p>
+  </a></div>
+
+  <div class="camp_stock"><a href="https://yoihibi.jp/item/ARUSUP.html" target="_blank">
+  <p class="camp_stock_img"><img src="https://dbcn1bdvswqbx.cloudfront.net/client_info/FLORAHOUSE01/itemimage/syouhin/aw12n00.jpg"></p>
+  <p class="camp_stock_txt">アルファウェーブ サポート腹巻</p>
+  </a></div>
+
+  <div class="camp_stock"><a href="https://yoihibi.jp/item/ARUSUP.html" target="_blank">
+  <p class="camp_stock_img"><img src="https://dbcn1bdvswqbx.cloudfront.net/client_info/FLORAHOUSE01/itemimage/syouhin/aw14n00.jpg"></p>
+  <p class="camp_stock_txt">アルファウェーブ スパッツ</p>
+  </a></div>
+
+</div>
