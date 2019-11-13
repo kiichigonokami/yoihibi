867ae14b82fd291853205cc154350b8bcf34d858
kiichigonokami <info@kiichigo.work>
2019-11-13 21:47:46 +0900

    fix

--- userweb/feature/1911KOE/index.php
+++ userweb/feature/1911KOE/index.php
@@ -1,6 +1,6 @@
 <?php
-$stock_id = '1910KOE';
-$site_title = '声結び 今年の冬はぽかぽかに過ごしましょう！質のよい遠赤外線で冷え知らず アルファウェーブ編';
+$stock_id = '1911KOE';
+$site_title = '声結び 今年の冬はぽかぽかに過ごしましょう！質のよい遠赤外線で冷え知らず サン・マット編';
 //$Path = 'https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1905KOE/';
 $Path = './img/';
 include('../loop.php');
--- userweb/feature/1911KOE/parts/2.php
+++ userweb/feature/1911KOE/parts/2.php
@@ -56,7 +56,7 @@
 .fl-r {display: block; width: 100%; margin: 0; padding: 0;}
 .l-2-3 { width: 100%; margin-bottom: 40px;}
 
-#toku-ttl2 { background: url('https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/sp12.jpg') center top 16% no-repeat; background-size: 100%;}
+#toku-ttl2 { background: url('https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1911KOE/sp12.jpg') center top 16% no-repeat; background-size: 100%;}
 
 /*おすすめ商品*/
 .camp_stock { width: 50%!important;}
@@ -76,77 +76,54 @@
 <!--★削除禁止★ -->
 
 <div class="column_head">
-  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/title.png" class="spnone">
-  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/title_s.png" class="pcnone">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1911KOE/title.png" class="spnone">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1911KOE/title_s.png" class="pcnone">
   <div class="intoroduction">
-寒さ対策は万全ですか？ 声結びでは、からだが芯から温まる遠赤外線商品について３号連続で特集します。今月は、電気を使わない「アルファウェーブ」。アルファウェーブは、「使い始めてライフスタイルが変わった」というお声をいただく人気シリーズです。他の皆さんの使い方を参考に、今年の冬は冷え知らずで過ごしませんか？
+  今月は、医療認可も受けている『サン・マット』をご紹介します。『サン・マット』は、医療機関向けの遠赤外線温熱治療器を、家庭でも使えるように作られたものです。身に着けているだけでからだをやさしく温めて、血液の巡りをよくします。低温やけどの心配もありませんので、寝たきりの方の温熱療法にもおすすめです。
   </div>
 </div>
 
 <div class="article_detail_mokuji">
   <p><img src="//d2mxjn1ymx4y84.cloudfront.net/client_info/FLORAHOUSE01/itemimage/1811OGAT/yomimono_mokuji.png" alt=""></p>
   <ul>
-    <a href="#toku-ttl1"><li>スクエアパッド｜5層キルティング構造で温かさが持続</li></a>
-    <a href="#toku-ttl2"><li>パイルケット｜夏はタオルケット、冬は毛布感覚で使える</li></a>
-    <a href="#toku-ttl4"><li>肩パッド・ロング｜肩と背中にしっかりフィット</li></a>
-    <a href="#toku-ttl5"><li>サポート腹巻｜薄手でよく伸び、お腹を包み込む</li></a>
-    <a href="#toku-ttl5"><li>スパッツ｜程よいフィット感で下半身から温める</li></a>
-    <a href="#toku-ttl3"><li>「アルファウェーブ」温かさのメカニズム</li></a>
+    <a href="#toku-ttl1"><li>サン・マット 敷きタイプ</li></a>
+    <a href="#toku-ttl2"><li>サン・マット 巻き付けタイプ</li></a>
+    <a href="#toku-ttl3"><li>『サン・マット』での集中温熱は、健康への近道！</li></a>
   </ul>
 </div>
 
 <div class="column_100img" id="toku-ttl1">
-	<img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/pc1.png" class="spnone" alt="スクエアパッド｜5層キルティング構造で温かさが持続｜アルファウエーブ">
-  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/sp1.png" class="pcnone" alt="スクエアパッド｜5層キルティング構造で温かさが持続｜アルファウエーブ">
+	<img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1911KOE/pc1.png" class="spnone" alt="サン・マット 敷きタイプ">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1911KOE/sp1.png" class="pcnone" alt="サン・マット 敷きタイプ">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1911KOE/sp1_2.png" class="pcnone" alt="サン・マット 敷きタイプ">
 </div>
 
 <div class="column_100img" id="toku-ttl2">
-  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/pc2.png" class="spnone" alt="パイルケット｜夏はタオルケット、冬は毛布感覚で使える｜アルファウエーブ">
-  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/sp2.png" class="pcnone" alt="パイルケット｜夏はタオルケット、冬は毛布感覚で使える｜アルファウエーブ">
-</div>
-
-<div class="column_100img" id="toku-ttl4">
-  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/pc4.png" class="spnone" alt="肩パッド・ロング｜肩と背中にしっかりフィット｜アルファウエーブ">
-  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/sp4.png" class="pcnone" alt="肩パッド・ロング｜肩と背中にしっかりフィット｜アルファウエーブ">
-</div>
-
-<div class="column_100img" id="toku-ttl5">
-  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/pc6.png" class="spnone" alt="サポート腹巻｜5層キルティング構造で温かさが持続｜スパッツ｜程よいフィット感で下半身から温める｜アルファウエーブ">
-  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/sp5.png" class="pcnone" alt="サポート腹巻｜5層キルティング構造で温かさが持続｜アルファウエーブ">
-  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/sp6.png" class="pcnone" alt="スパッツ｜程よいフィット感で下半身から温める｜アルファウエーブ">
-  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/sp7.png" class="pcnone" alt="">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1911KOE/pc2.png" class="spnone" alt="サン・マット 巻き付けタイプ">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1911KOE/sp2.png" class="pcnone" alt="サン・マット 巻き付けタイプ">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1911KOE/sp2_2.png" class="pcnone" alt="サン・マット 巻き付けタイプ">
 </div>
 
 <div class="column_100img" id="toku-ttl3">
-  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/pc3.png" class="spnone" alt="アルファウェーブ」温かさのメカニズム">
-  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1910KOE/sp3.png" class="pcnone" alt="アルファウェーブ」温かさのメカニズム">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1911KOE/pc3.png" class="spnone" alt="『サン・マット』での集中温熱は、健康への近道！">
+  <img src="https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1911KOE/sp3.png" class="pcnone" alt="『サン・マット』での集中温熱は、健康への近道！">
 </div>
 
 <div class="camp_stock_area">
 
+  <div class="camp_stock"><a href="https://yoihibi.jp/item/SUNMAT.html" target="_blank">
+  <p class="camp_stock_img"><img src="https://dbcn1bdvswqbx.cloudfront.net/client_info/FLORAHOUSE01/itemimage/syouhin/sunmat_camp.jpg"></p>
+  <p class="camp_stock_txt">サン・マット 各種</p>
+  </a></div>
+
   <div class="camp_stock"><a href="https://yoihibi.jp/item/ARUSQP.html" target="_blank">
   <p class="camp_stock_img"><img src="https://dbcn1bdvswqbx.cloudfront.net/client_info/FLORAHOUSE01/itemimage/syouhin/arusqp.jpg"></p>
-  <p class="camp_stock_txt">アルファウェーブ スクエアパッド</p>
-  </a></div>
-
-  <div class="camp_stock"><a href="https://yoihibi.jp/item/ARUPAI.html" target="_blank">
-  <p class="camp_stock_img"><img src="https://dbcn1bdvswqbx.cloudfront.net/client_info/FLORAHOUSE01/itemimage/syouhin/arupai.jpg"></p>
-  <p class="camp_stock_txt">アルファウェーブ パイルケット</p>
-  </a></div>
-
-  <div class="camp_stock"><a href="https://yoihibi.jp/item/ARUKAT.html" target="_blank">
-  <p class="camp_stock_img"><img src="https://dbcn1bdvswqbx.cloudfront.net/client_info/FLORAHOUSE01/itemimage/syouhin/arukat.jpg"></p>
-  <p class="camp_stock_txt">アルファウェーブ 肩パッド・ロング</p>
+  <p class="camp_stock_txt">アルファウェーブスクエアパッド</p>
   </a></div>
 
   <div class="camp_stock"><a href="https://yoihibi.jp/item/ARUSUP.html" target="_blank">
   <p class="camp_stock_img"><img src="https://dbcn1bdvswqbx.cloudfront.net/client_info/FLORAHOUSE01/itemimage/syouhin/aw12n00.jpg"></p>
   <p class="camp_stock_txt">アルファウェーブ サポート腹巻</p>
-  </a></div>
-
-  <div class="camp_stock"><a href="https://yoihibi.jp/item/ARUSUP.html" target="_blank">
-  <p class="camp_stock_img"><img src="https://dbcn1bdvswqbx.cloudfront.net/client_info/FLORAHOUSE01/itemimage/syouhin/aw14n00.jpg"></p>
-  <p class="camp_stock_txt">アルファウェーブ スパッツ</p>
   </a></div>
 
 </div>
