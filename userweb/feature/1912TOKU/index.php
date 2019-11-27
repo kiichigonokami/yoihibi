da5070e6e80dad23c9a66c1b0207eda971113f00
kiichigonokami <info@kiichigo.work>
2019-11-28 02:04:10 +0900

    fix

---
+++ userweb/feature/1912TOKU/index.php
@@ -0,0 +1,7 @@
+<?php
+$stock_id = '1907RECI';
+$site_title = '薬膳で夏バテ対策';
+$Path = 'https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1907RECI/';
+//$Path = './img/';
+include('../loop.php');
+?>
