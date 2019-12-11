50f771a54497ab9e9e4d6fb99a3dabc2c39bba35
kiichigonokami <info@kiichigo.work>
2019-12-12 00:46:46 +0900

    fix

---
+++ userweb/feature/1912KOE/index.php
@@ -0,0 +1,7 @@
+<?php
+$stock_id = '1912KOE';
+$site_title = '声結び 今年の冬はぽかぽかに過ごしましょう！質のよい遠赤外線で冷え知らず アルファウェーブ編';
+//$Path = 'https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1905KOE/';
+$Path = './img/';
+include('../loop.php');
+?>
