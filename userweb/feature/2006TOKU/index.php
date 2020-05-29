ea3bdd4406b4c714c1c5d6fce111dc51f7d5a28c
kiichigo <kiichigo@MacBook-Pro.local>
2020-05-29 15:49:35 +0900

    fix

---
+++ userweb/feature/2006TOKU/index.php
@@ -0,0 +1,8 @@
+<?php
+$stock_id = '2006TOKU';
+$site_title = '食生活が免疫力に差をつける';
+//$Path = 'https://aladdinec.jp/client_info/FLORAHOUSE01/view/userweb/images/feature/1911TOKU/';
+//$cam = '';
+$Path = './img/';
+include('../loop.php');
+?>
---
+++ userweb/feature/2006TOKU/parts/1.php
@@ -0,0 +1,26 @@
+<div id="kiji_prof" id="toku-ttl5">
+  <!--PC用-->
+  <div class="kiji_prof_area spnone">
+    <div class="kiji_prof_disp">
+      <span>氏家京子</span>
+      <small>（うじいえ・きょうこ）</small>さん<br>
+      <br>
+      <b>食生活・医学ジャーナリスト</b><br>
+      国内と海外の優れた栄養療法や代替療法などを取材し、執筆、翻訳、講演を行うほか、免疫機能を高める食事療法のオンライン講座やワークショップも開催。最新刊『細胞から回復する 高カリウム低ナトリウム療法』ネクストパブリッシング・オーサーズプレス。
+    </div>
+    <div class="kiji_prof_img">
+      <img src="<?php echo $Path ?>prof.png">
+    </div>
+  </div>
+
+  <!--スマホ用-->
+  <div class="kiji_prof_area pcnone">
+    <span>氏家京子</span><small>（うじいえ・きょうこ）</small>さん<br>
+    <br>
+    <img src="<?php echo $Path ?>prof.png">
+    <br>
+    <br>
+    <b>食生活・医学ジャーナリスト</b><br>
+    国内と海外の優れた栄養療法や代替療法などを取材し、執筆、翻訳、講演を行うほか、免疫機能を高める食事療法のオンライン講座やワークショップも開催。最新刊『細胞から回復する 高カリウム低ナトリウム療法』ネクストパブリッシング・オーサーズプレス。
+  </div>
+</div>
